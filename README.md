6b721461bbfd1da9375580f47669f33321fbadb4
kkocdko <kkocdko@163.com>
2023-07-16 03:11:14 +0800

    Load wolfSSL example server-tls.c

---
+++ README.md
@@ -0,0 +1,33 @@
+# wolfssl-util
+
+Some kkocdko's crazy ideas about TLS & HTTPS supports in async Rust.
+
+## Goals
+
+- TlsStream (tokio).
+
+- HttpsConnector (hyper).
+
+- Single crate, less dependencies.
+
+<!--
+https://stackoverflow.com/questions/50887018/cmake-generate-single-header-file
+https://github.com/rust-lang/cc-rs/issues/242
+https://www.wolfssl.com/documentation/manuals/wolfssl/chapter02.html
+先尝试 bindgen 或者其他方案
+crate “cc”
+绑定代码？询问
+-->
+
+<!--
+# openssl
+mkdir -p target
+cd target
+curl -o openssl.tar.gz -L https://github.com/openssl/openssl/releases/download/openssl-3.1.1/openssl-3.1.1.tar.gz
+rm -rf openssl
+mkdir openssl
+tar -xf openssl.tar.gz --strip-components 1 -C openssl
+cd openssl
+rm -rf test doc demos CHANGES.md
+tar -cJf openssl.tar.xz openssl
+-->
---
+++ project.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+cd $(dirname $(realpath $0))
+mkdir -p target
+
+if [ "$1" = "init-wolfssl" ]; then
+  cd target
+  if [ ! -e wolfssl.tar.gz ]; then
+    curl -o wolfssl.tar.gz -L https://github.com/wolfSSL/wolfssl/archive/refs/tags/v5.6.3-stable.tar.gz
+    # curl -o wolfssl.tar.gz -L https://github.com/wolfSSL/wolfssl/archive/refs/heads/master.tar.gz
+  fi
+  rm -rf wolfssl
+  mkdir wolfssl
+  tar -xf wolfssl.tar.gz --strip-components 1 -C wolfssl
+  cd wolfssl
+  rm -rf build
+  # cmake -B build -G Ninja -D CMAKE_BUILD_TYPE=MinSizeRel
+  # cmake -B build -G Ninja -D WOLFSSL_OLD_TLS=no -D WOLFSSL_BASE64_ENCODE=no -D WOLFSSL_MD5=no -D WOLFSSL_EXAMPLES=no -D WOLFSSL_DTLS=no -D WOLFSSL_ECC=no -D CMAKE_BUILD_TYPE=MinSizeRel
+  # cmake --build build
+  # ls -lh /home/kkocdko/misc/code/wolfssl-util/target/wolfssl/build/libwolfssl.so.35.5.1
+  ./autogen.sh
+  # --disable-filesystem
+  ./configure --disable-oldtls --disable-examples --disable-md5 --disable-sp --enable-alpn --disable-base64encode --disable-shared
+  make -j`nproc`
+  ls -lh /home/kkocdko/misc/code/wolfssl-util/target/wolfssl/src/.libs/libwolfssl.so.35.5.1
+  # --disable-errorstrings
+  # https://www.wolfssl.com/documentation/manuals/wolfssl/chapter02.html#build-options
+  # sudo dnf install autoconf automake libtool
+  exit
+fi
+
+if [ "$1" = "run-wolfssl" ]; then
+  ~/misc/apps/mold -run g++ src/main.cc -o target/main -I target/wolfssl -L target/wolfssl/src/.libs -lwolfssl -Wall -Wextra -g -fsanitize=address,undefined -fno-omit-frame-pointer
+  target/main
+  exit
+fi
